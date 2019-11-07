aa2255bf370b5e93b8a131a12078fb4c19227e21
lydia delice <delydia84@gmail.com>
2019-11-07 21:23:32 +0200

    add develop branch

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -18,6 +18,15 @@ const user2 = {
     username: 'regis',
     password: '123456',
 };
-users.push(user1, user2);
+const user3 = {
+    id: 3,
+    firstname: 'kaliza',
+    lastname: 'ange',
+    email: 'ange@gmail.com',
+    phoneNumber: '078012516',
+    username: 'ange',
+    password: '123456',
+};
+users.push(user1, user2, user3);
 
 export default users;
