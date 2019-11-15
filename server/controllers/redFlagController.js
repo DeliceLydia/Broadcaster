5f2bf6fc505e384993b1ae57a391690ac269a45d
lydia delice <delydia84@gmail.com>
2019-11-15 12:09:37 +0200

    ft(server): update comment add update comment endpoint
    [Delivers #169776455]

--- server/controllers/redFlagController.js
+++ server/controllers/redFlagController.js
@@ -1,52 +1,64 @@
-import {validateRedFlag, validateLocation} from '../validations/redFlagValidation';
+import { validateRedFlag, validateLocation, validateComment } from '../validations/redFlagValidation';
 import responseMessage from '../helpers/response';
 import moment from 'moment';
 import redFlags from '../models/redFlagModel';
-import users from '../models/usersModel';
 
 
 class RedFlags {
-    static postRedFlag(req, res) {
-        const { error } = validateRedFlag.validation(req.body);
+  static postRedFlag(req, res) {
+    const { error } = validateRedFlag.validation(req.body);
     if (error) { return responseMessage.errorMessage(res, 400, error.details[0].message); }
-    
+
     const redFlagId = redFlags.length + 1;
-    const {title, type, comment, location, status} = req.body;
-    const redFlag = {redFlagId, created_on : moment().format('LL'), title, type, comment, createdBy : req.user.email, location, status,};
+    const { title, type, comment, location, status } = req.body;
+    const redFlag = { redFlagId, created_on: moment().format('LL'), title, type, comment, createdBy: req.user.email, location, status, };
     redFlags.push(redFlag);
-    return responseMessage.successWithData(res, 200, 'Created red flag record', {redFlagId});
-   }
-   
-   static getAll(req, res) {
+    return responseMessage.successWithData(res, 200, 'Created red flag record', { redFlagId });
+  }
+
+  static getAll(req, res) {
     if (!redFlags) { return responseMessage.errorMessage(res, 404, 'no red flags found'); }
-    else { return responseMessage.successUser(res, 200 , redFlags); }
-   }
-   
-   static getOne(req, res) {
+    else { return responseMessage.successUser(res, 200, redFlags); }
+  }
+
+  static getOne(req, res) {
     const redFlagId = redFlags.find(h => h.redFlagId === parseInt(req.params.redFlagId));
-    if (!redFlagId) { return responseMessage.errorMessage(res, 404, 'red flag not found')}
+    if (!redFlagId) { return responseMessage.errorMessage(res, 404, 'red flag not found') }
     else { return responseMessage.successUser(res, 200, redFlagId); }
   }
-  
+
   static updateLocation(req, res) {
     const { error } = validateLocation.validation(req.body);
     if (error) { return responseMessage.errorMessage(res, 400, error.details[0].message); }
     const check_redFlag = redFlags.find(i => i.redFlagId === parseInt(req.params.redFlagId));
-    
-    if(!check_redFlag) 
-    { return responseMessage.errorMessage(res, 404, 'red flag not found')}
-     
-    else if(check_redFlag.status !== 'draft') 
-    { return responseMessage.errorMessage(res, 400, 'you are not allowed to change the location')}
-    
-    else if(check_redFlag.createdBy !== req.user.email) {
+
+    if (!check_redFlag) { return responseMessage.errorMessage(res, 404, 'red flag not found') }
+
+    else if (check_redFlag.status !== 'draft') { return responseMessage.errorMessage(res, 400, 'you are not allowed to change the location') }
+
+    else if (check_redFlag.createdBy !== req.user.email) {
       return responseMessage.errorMessage(res, 400, 'this record does not belong to you')
-    } 
-    else if(check_redFlag.createdBy === req.user.email){
-      check_redFlag.location = req.body.location;
-      return responseMessage.successWithData(res, 200, "updated red-flag record's location ", {redFlagId : check_redFlag.redFlagId})
     }
-   
-   }
-}
+    else if (check_redFlag.createdBy === req.user.email) {
+      check_redFlag.location = req.body.location;
+      return responseMessage.successWithData(res, 200, "updated red-flag record's location ", { redFlagId: check_redFlag.redFlagId })
+    }
+ }
+  static updateComment(req, res) {
+    const { error } = validateComment.validation(req.body);
+    if (error) { return responseMessage.errorMessage(res, 400, error.details[0].message); }
+    const check_Flag = redFlags.find(i => i.redFlagId === parseInt(req.params.redFlagId));
+
+    if (!check_Flag) { return responseMessage.errorMessage(res, 404, 'red flag not found') }
+
+    else if (check_Flag.createdBy !== req.user.email) {
+      return responseMessage.errorMessage(res, 400, 'this record does not belong to you')
+    }
+    else if (check_Flag.createdBy === req.user.email) {
+      check_Flag.comment = req.body.comment;
+      return responseMessage.successWithData(res, 200, "updated red-flag record's comment ", { redFlagId: check_Flag.redFlagId })
+    }
+  }
+  }
+  
 export default RedFlags;
