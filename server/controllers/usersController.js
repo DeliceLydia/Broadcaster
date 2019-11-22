823d2e0fb3f3ee7c72fad89f5b2c96df14393d58
lydia delice <delydia84@gmail.com>
2019-11-22 21:47:46 +0200

    fixed tests

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,45 +1,50 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import userValidations from '../validations/userValidations';
+import {validateSignup,validateSignin} from '../validations/userValidations';
 import users from '../models/usersModel';
 import responseMessage from '../helpers/response';
 
 dotenv.config();
 
 class Users {
-    static signup(req, res) {
-        if (userValidations.validateSignup(req, res)) {
+    static signup(req,res) {
+            const { error } = validateSignup.validation(req.body);
+            if (error) {
+                return responseMessage.errorMessage(res, 400, error.details[0].message);
+            }
             const user = users.find(u => u.email === req.body.email);
             if (user) {
                 return responseMessage.errorMessage(res, 401, 'Email already exist');
-            }
-        }
-
-        const hash = bcrypt.hashSync(req.body.password.trim(), 10);
-        const id = parseInt(users.length + 1);
-        const { firstname, lastname, email, phoneNumber, username } = req.body;
-        const newUser = { id, firstname, lastname, email, phoneNumber, username, password: hash };
-        const payload = {
-            id: newUser.id,
-            firstname: newUser.firstname,
-            lastname: newUser.lastname,
-            email: newUser.email
-        }
-        const token = jwt.sign(payload, 'SECRET_KEY', { expiresIn: '24hrs' });
-        users.push(newUser);
-        return responseMessage.successWithData(res, 201, 'User created successfully', { token });
+                }
+                const hash = bcrypt.hashSync(req.body.password.trim(),10);
+                const id = parseInt(users.length + 1);
+                const{firstname, lastname, email, phoneNumber, username} = req.body;
+                const newUser = {id, firstname, lastname, email, phoneNumber, username, password: hash};
+                const payload = {
+                    id: newUser.id,
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    email: newUser.email
+                }
+                const token = jwt.sign(payload, 'SECRET_KEY', { expiresIn: '24hrs' });
+                users.push(newUser);
+                return responseMessage.successWithData(res, 201, 'User created successfully', {token});
     }
-    static signin(req, res) {
-            const findUser = users.find(u => u.email === req.body.email);
-            if (!findUser) { return responseMessage.errorMessage(res, 400, 'incorrect email or password'); }
-        const password = bcrypt.compareSync(req.body.password.trim(), findUser.password);
-        if (!password) {
-            return responseMessage.errorMessage(res, 400, 'incorrect email or password');
-        }
-        const payload = { id: findUser.id, firstname: findUser.firstname, lastname: findUser.lastname, email: findUser.email }
-        const token = jwt.sign(payload, 'SECRET_KEY', { expiresIn: '1d' });
-        return responseMessage.successWithData(res, 200, 'User is successfully logged in!', { token });
-   }
+    static signin(req,res) {
+        const { error } = validateSignin.validation(req.body);
+            if (error) {
+                return responseMessage.errorMessage(res, 400, error.details[0].message);}
+            const user = users.find(u => u.email === req.body.email);
+            
+            if(!user) {return responseMessage.errorMessage(res, 400, 'incorrect email or password');}
+            const password = bcrypt.compareSync(req.body.password.trim(), user.password);
+            
+            if (!password) {
+            return responseMessage.errorMessage(res, 400, 'incorrect email or password');}
+    const payload = {id: user.id, firstname: user.firstname, lastname: user.lastname, email: user.email}
+    const token = jwt.sign(payload, 'SECRET_KEY', { expiresIn: '1d' });
+    return responseMessage.successWithData(res, 200, 'User is successfully logged in!', {token});
+  }
 }
 export default Users;
--- server/validations/userValidations.js
+++ server/validations/userValidations.js
@@ -1,89 +1,28 @@
-import validator from 'validator';
+import joi from 'joi';
 
-class userValidations {
-    static validateSignup(req, res) {
-
-        if (!typeof req.body.firstname === 'string') {
-            throw Error('firstname must be string');
-        }
-        if (validator.isEmpty(req.body.firstname)) {
-            throw Error('firstname is required');
-        }
-        if (!validator.isAlphanumeric(req.body.firstname)) {
-            throw Error('firstname must not contain special characters');
-        }
-        if (validator.isNumeric(req.body.firstname)) {
-            throw Error('firstname must be string');
-        }
-        if (validator.isEmpty(req.body.lastname)) {
-            throw Error('lastname is required');
-        }
-        if (validator.isNumeric(req.body.lastname)) {
-            throw Error('lastname must be string');
-        }
-        if (!validator.isAlphanumeric(req.body.lastname)) {
-            throw Error('lastname must not contain special characters');
-        }
-        if (typeof req.body.email === 'number') {
-            throw Error('your email must look like  this ex:lydia@gmail.com');
-        }
-        if (validator.isEmpty(req.body.email)) {
-            throw Error('email is required');
-        }
-        if (!validator.isEmail(req.body.email)) {
-            throw Error('your email must look like  this ex:lydia@gmail.com');
-        }
-        if (validator.isEmpty(req.body.phoneNumber)) {
-            throw Error('phoneNumber is required');
-        }
-        if (!validator.isNumeric(req.body.phoneNumber)) {
-            throw Error('phoneNumber must be a number');
-        }
-        if (!validator.isAlphanumeric(req.body.phoneNumber)) {
-            throw Error('phoneNumber must not contain special characters');
-        }
-        if (!validator.isLength(req.body.phoneNumber, { min: 8, max: 10 })) {
-            throw Error('phoneNumber is too short');
-        }
-        if (validator.isEmpty(req.body.username)) {
-            throw Error('username is required');
-        }
-        if (validator.isNumeric(req.body.username)) {
-            throw Error('username must be string');
-        }
-        if (!validator.isAlphanumeric(req.body.username)) {
-            throw Error('username must not contain special characters');
-        }
-        if (validator.isEmpty(req.body.password)) {
-            throw Error('password is required');
-        }
-        if (!validator.isLength(req.body.password, { min: 6, max: 250 })) {
-            throw Error('password is too short');
-        }
-        if (validator.isAlphanumeric(req.body.password)) {
-            throw Error('password must contain some special characters');
-        } else {
-
-        }
-        return true;
-    }
-    static validatesignin(req, res) {
-        if (typeof req.body.email === 'number') {
-            throw Error('your email must look like  this ex:lydia@gmail.com');
-        }
-        if (validator.isEmpty(req.body.email)) {
-            throw Error('email is required');
-        }
-        if (!validator.isEmail(req.body.email)) {
-            throw Error('your email must look like  this ex:lydia@gmail.com');
-        }
-        if (validator.isEmpty(req.body.password)) {
-            throw Error('password is required');
-        } else {
-
-        }
-        return true;
+const validateSignup = {
+    validation(signup) {
+        const schema = {
+            firstname: joi.string().trim().required(),
+            lastname: joi.string().trim().required(),
+            email: joi.string().email({ minDomainAtoms: 2 }).trim().required(),
+            phoneNumber: joi.string().trim().required(),
+            username: joi.string().trim().required(),
+            password: joi.string().min(6).max(10).required(),
+        };
+        return joi.validate(signup, schema);
     }
 }
-export default userValidations;
-
+    const validateSignin = {
+        validation(returningUser) { 
+            const schema = {
+              email: joi.string().email().required(),
+              password: joi.string().required(),
+            };
+            return joi.validate(returningUser, schema);
+          }
+        }
+    export {
+        validateSignup,
+        validateSignin,
+    }
