6e959b9f8b9620b62c8bf9c6e4e0548a4bf4529b
lydia <Delydia84@gmail.com>
2019-12-05 11:19:37 +0200

    heroku hosting

--- v1/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import Users from '../controllers/usersController';
-
-const user_router = express.Router();
-
-user_router.post('/api/v1/auth/signup', Users.signup);
-user_router.post('/api/v1/auth/signin', Users.signin);
-
-export default user_router;
